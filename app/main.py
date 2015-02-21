637fae472f5f213aad228dbba798eaa4b290d64a
user <user@w87-188-093.wireless.uvic.ca>
2015-02-21 14:53:38 -0800

    snake loc

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-    print data["snakes"]
+    print data["snakes"][0]["coords"]
     print data["food"]
     
     return json.dumps({
