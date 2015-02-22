432e62c4bea618228e829dd8d1f21abb8c533ec7
Tehrm <tom.e.gracie@gmail.com>
2015-02-21 16:06:34 -0800

    fixing coord format?

--- app/main.py
+++ app/main.py
@@ -27,8 +27,8 @@ def start():
 def move():
     data = bottle.request.json
 
-    #print data['snakes']
-    #print data['food']
+    print data['snakes']
+    print data['food']
 
     oursnake = 0
 
