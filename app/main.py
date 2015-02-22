29fae90a7e321c3ad71fa7b95651eaf37e6b8b97
user <user@w87-188-093.wireless.uvic.ca>
2015-02-21 16:05:34 -0800

    snake accumulation works

--- app/main.py
+++ app/main.py
@@ -4,70 +4,65 @@ import json
 
 @bottle.get('/')
 def index():
-    return """
-        <a href="https://github.com/sendwithus/battlesnake-python">
-            battlesnake-python
-        </a>
-    """
+		return """
+				<a href="https://github.com/sendwithus/battlesnake-python">
+						battlesnake-python
+				</a>
+		"""
 
 
 @bottle.post('/start')
 def start():
-    data = bottle.request.json
+		data = bottle.request.json
 
-    return json.dumps({
-        'name': 'aspkickers',
-        'color': '#00ffff',
-        'head_url': 'http://i.imgur.com/jhitWnu.png',
-        'taunt': 'There\'s a snake in my boot!'
-    })
+		return json.dumps({
+				'name': 'aspkickers',
+				'color': '#00ffff',
+				'head_url': 'http://i.imgur.com/jhitWnu.png',
+				'taunt': 'There\'s a snake in my boot!'
+		})
 
 
 @bottle.post('/move')
 def move():
-    data = bottle.request.json
+		data = bottle.request.json
 
-    #print data['snakes']
-    #print data['food']
+		#print data['snakes']
+		#print data['food']
 
-    oursnake = 0
+		oursnake = 0
 
-    for snake in data["snakes"]:
-      if(snake.name) == 'aspkickers':
-        oursnake = snake
-
-
-    print data['snakes'][oursnake]['coords']
-    print data['food']
-
-    
-    all_snakes = []
-    # for snake in snakes
-    for snake in data['snakes']:
-      for coord in snake['coords']:
-        all_snakes.append(coord)
-
-    print all_snakes
-      
-
-    # get coordinates 
-    # accumulate coordinates
+		for snake in data["snakes"]:
+			if(snake.name) == 'aspkickers':
+				oursnake = snake
 
 
-    return json.dumps({
-        'move': 'right',
-        'taunt': 'You\'re my favourite deputy!'
-    })
+		print data['snakes'][oursnake]['coords']
+		print data['food']
+
+		## #CORRESSSCTCTCTCTCTTTT
+		all_snakes = []
+		# for snake in snakes
+		for snake in data['snakes']:
+				for coord in snake['coords']:
+						all_snakes.append(coord)
+
+		print all_snakes
+
+		return json.dumps({
+				'move': 'right',
+				'taunt': 'You\'re my favourite deputy!'
+		})
 
 def isWall(point):
-    x_co = point[]
+		x_co = point[]
 
 
 @bottle.post('/end')
 def end():
-    data = bottle.request.json
+		data = bottle.request.json
 
-    return json.dumps({})
+		return json.dumps({})
 
 
 # Expose WSGI app
